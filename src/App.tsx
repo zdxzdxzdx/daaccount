0ef72af0db22161f183ddfecf7ad200ea3323ce7
zdxzdxzdx <769371987@qq.com>
2021-02-22 11:15:08 +0800

    导航栏完成

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,100 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from 'react-router-dom';
 import styled from 'styled-components';
 
+
+const Wrapper = styled.div
+  `
+   
+   display: flex;
+   flex-direction: column;
+   height: 100vh;
+`;
+
+const Main = styled.div`
+    flex-grow: 1; 
+    overflow: auto;
+`
+const Nav =styled.nav`
+  >ul{
+   display: flex;
+    >li{
+    width: 33.333%;
+    text-align: center;
+    padding: 16px;
+    }
+ 
+ 
+ }
+ 
+
+`
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Wrapper>
+      <Main>
+        <Switch>
+          <Route path="/money">
+            <Money/>
+          </Route>
+
+          <Route path="/tags">
+            <Tags/>
+          </Route>
+
+          <Route path="/statistics">
+            <Statistics/>
+          </Route>
+
+          <Redirect exact from="/" to="/money"/>
+
+          <Route path="*">
+            <NoMatch/>
+          </Route>
+        </Switch>
+      </Main>
+        <Nav>
+          <ul>
+            <li>
+              <Link to="/money">记账</Link>
+            </li>
+            <li>
+              <Link to="/tags">标签</Link>
+            </li>
+            <li>
+              <Link to="/statistics">统计</Link>
+            </li>
+          </ul>
+        </Nav>
+      </Wrapper>
+    </Router>
   );
+}
+
+function NoMatch() {
+  return <div>
+    <h2>no match</h2>
+  </div>;
+}
+
+function Money() {
+  return <h2>money</h2>;
+}
+
+function Tags() {
+  return <h2>tags</h2>;
+}
+
+function Statistics() {
+  return <h2>statistics</h2>;
 }
 
 export default App;
