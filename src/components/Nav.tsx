40b8499e40002a0a8d8fe5d401e50fc258bb85f8
zdxzdxzdx <769371987@qq.com>
2021-02-22 14:03:42 +0800

    引入三个icons

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,36 +2,59 @@ import styled from 'styled-components';
 import {Link} from 'react-router-dom';
 import React from 'react';
 
-const NavWrapper =styled.nav`
+require('icons/money.svg');
+require('icons/label.svg');
+require('icons/statistics.svg');
+
+
+const NavWrapper = styled.nav`
     border: 1px solid red;
     line-height: 24px;
     box-shadow: 0 0 3px rgba(0,0,0,0.25);
   >ul{
    display: flex;
     >li{
-    width: 33.333%;
+    width: 33.3333%;
     text-align: center;
-    padding: 16px;
+    display: flex;
+    flex-direction: column;
+    padding: 4px  0;
+    justify-content: center;
+    align-items: center;
+    .icon{
+        width: 24px;
+        height: 24px;
     }
- }`
-const Nav = ()=>{
+    
+    }
+ }`;
+const Nav = () => {
   return (
     <NavWrapper>
       <ul>
         <li>
+          <svg fill="red" className="icon">
+            <use xlinkHref="#money"/>
+          </svg>
           <Link to="/money">记账</Link>
         </li>
         <li>
+          <svg fill="red" className="icon">
+            <use xlinkHref="#label"/>
+          </svg>
           <Link to="/tags">标签</Link>
         </li>
         <li>
+          <svg fill="red" className="icon">
+            <use xlinkHref="#statistics"/>
+          </svg>
           <Link to="/statistics">统计</Link>
         </li>
       </ul>
 
     </NavWrapper>
 
-  )
-}
+  );
+};
 
-export default  Nav;
+export default Nav;
