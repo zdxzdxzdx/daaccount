52ec874f7223d6db2e154f063b9f212aadf005b4
zdxzdxzdx <769371987@qq.com>
2021-02-23 09:46:10 +0800

    Money.tsx四个组件模块化

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -1,118 +1,10 @@
 import Layout from '../components/Layout';
 import React from 'react';
 import styled from 'styled-components';
-
-const TagSection = styled.section`
-      background: #FFFFFF;
-      flex-grow: 1;
-      padding: 12px 16px;
-      display: flex;
-      flex-direction: column;
-      justify-content: flex-end;
-      align-items: flex-start;
-      > ol {
-         margin: 0 -12px;
-         li{
-         background: #D9D9D9;
-         border-radius: 18px;
-         display: inline-block;
-         padding: 3px 18px;
-         font-size: 14px;
-         margin: 8px 12px;
-         }
-      }
-      > button{
-        background: none;
-        border: none;
-        padding: 2px 4px;
-        border-bottom: 1px solid #333;
-        color: #666;
-        margin-top: 8px;
-      }
-
-`;
-const NotesSection = styled.section`
-   background: #f5f5f5;
-   padding: 0 16px;
-   font-size: 14px;
-    > label{
-        display: flex;
-        align-items: center;
-      > span {
-          margin-right: 16px;
-          white-space: nowrap;
-      }
-      > input {
-          display: block;
-          width: 100%;
-          height: 72px;
-          background: none;
-          border: none;
-      }
-    }
-`;
-const CategorySection = styled.section`
-    font-size: 24px;
-    > ul{
-        display: flex;
-        background: #c4c4c4;
-        
-        > li {
-          width: 50%;
-          text-align: center;
-          padding: 16px 0;
-          position: relative;
-          //background: #F0DE38;
-          background: aliceblue;
-           &.selected::after{
-              content: '';
-              display: block;
-              height: 3px;
-              background: #333;
-              position: absolute;
-              bottom: 0;
-              width: 100%;
-              left: 0;
-              
-           }
-          
-        }
-    }
-
-`;
-const NumberSection = styled.section`
-    display: flex;
-    flex-direction: column;
-    >.output{
-      background: white;
-      font-size: 36px;
-      line-height: 72px;
-      text-align: right;
-      padding: 0 16px;
-      box-shadow: inset 0 -5px 5px -5px rgba(0,0,0,0.25),
-                  inset 0 5px 5px -5px rgba(0,0,0,0.25);
-    }
-    >.pad{
-    >button{
-        font-size: 18px;
-        float: left;
-        width: 25%;
-        height: 64px;
-        border: none;
-        background: #F2F2F2;
-    &.OK{
-         height: 128px;
-         float: right; 
-    }
-    &.zero{
-        width: 50%;
-    }
-    }
-    
-    }
-   
-
-`;
+import {NotesSection} from './Money/NotesSection';
+import {CategorySection} from './Money/CategorySection';
+import {NumberSection} from './Money/NumberSection';
+import {TagSection} from './Money/TagSection';
 
 const MyLayout = styled(Layout)`
     display: flex;
@@ -131,7 +23,6 @@ function Money() {
         </ol>
         <button>新增标签</button>
       </TagSection>
-
       <NotesSection>
         <label>
           <span>备注</span>
