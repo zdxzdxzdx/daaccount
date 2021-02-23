64e00b44d7e0f12730f4adc18d311b7f25055dea
zdxzdxzdx <769371987@qq.com>
2021-02-23 17:01:31 +0800

    重构numberPad

--- src/views/Money/NumberSection.tsx
+++ src/views/Money/NumberSection.tsx
@@ -1,95 +1,34 @@
-import styled from 'styled-components';
+
 import React, {useState} from 'react';
+import {Wrapper} from './NumberPadSection/Wrapper';
+import {generatorOutput} from './NumberPadSection/generateOutput';
 
 
-const Wrapper = styled.section`
-    display: flex;
-    flex-direction: column;
-    >.output{
-      background: white;
-      font-size: 36px;
-      line-height: 72px;
-      text-align: right;
-      padding: 0 16px;
-      box-shadow: inset 0 -5px 5px -5px rgba(0,0,0,0.25),
-                  inset 0 5px 5px -5px rgba(0,0,0,0.25);
+const NumberSection: React.FC = () => {
+  const [output, _setOutput] = useState('0');
+  const setOutput = (output: string) => {
+    if (output.length > 16) {
+      output = output.slice(0, 16);
+    } else if (output.length === 0) {
+      output = '0';
     }
-    >.pad{
-    >button{
-        font-size: 18px;
-        float: left;
-        width: 25%;
-        height: 64px;
-        border: none;
-        background: #F2F2F2;
-    &.OK{
-         height: 128px;
-         float: right; 
+    _setOutput(output);
+  };
+
+  const onClickButtonWrapper = (e: React.MouseEvent) => {
+    const text = (e.target as HTMLButtonElement).textContent;
+    if (text === null) {
+      return;
     }
-    &.zero{
-        width: 50%;
+    if (text === 'OK') {
+      //TODO
+      return;
     }
+    if ('123456789.'.split('').concat(['删除', '清空']).indexOf(text) >= 0) {
+      // @ts-ignore
+      setOutput(generatorOutput(text, output));
     }
-    }
-  
-`;
-
-const NumberSection: React.FC=()=>{
-  const [output, _setOutput]= useState('0');
-  const setOutput =(output: string)=>{
-    if (output.length > 16){
-      output= output.slice(0,16)
-    }else if (output.length ===0){
-      output= '0'
-    }
-    _setOutput(output)
-
-  }
-
- const onClickButtonWrapper=(e: React.MouseEvent)=>{
-   const text = (e.target as HTMLButtonElement).textContent
-   if (text === null){return}
-   switch (text) {
-     case '0':
-     case '1':
-     case '2':
-     case '3':
-     case '4':
-     case '5':
-     case '6':
-     case '7':
-     case '8':
-     case '9':
-       if (output === '0'){
-         setOutput(text)}
-       else {
-         setOutput(output+text);
-       }
-       break
-     case '删除':
-          if (output.length === 1){
-            setOutput('');
-          }else{
-           setOutput( output.slice(0,-1))
-          }
-          break
-     case '清空':
-       setOutput('')
-       break
-     case 'OK':
-       console.log("OK");
-       break
-
-     case '.':
-        if (output.indexOf('.')>=0){
-          return;
-        }
-        setOutput(output+'.')
-
-       break
-
-   }
- }
+  };
 
 
   return <Wrapper>
@@ -112,6 +51,6 @@ const NumberSection: React.FC=()=>{
       <button className="zero">0</button>
       <button className="dot">.</button>
     </div>
-  </Wrapper>
-}
-export {NumberSection}
+  </Wrapper>;
+};
+export {NumberSection};
