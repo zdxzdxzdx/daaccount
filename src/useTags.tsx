4103bb65ed32d96334d9da3ae15f4861ab4b3cbd
zdxzdxzdx <769371987@qq.com>
2021-02-24 11:32:20 +0800

    Tags页面样式

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -1,7 +1,7 @@
 import {useState} from 'react';
 
 const useTags = ()=>{
-  const [tags, setTags] = useState<string[]>(['衣', '食', '住', '行']);
+  const [tags, setTags] = useState<string[]>(['衣涉及到很多女的你那绝后的那就按款项明细降低额爱德华健身卡很明显知识竞赛', '食', '住', '行']);
   return {tags,setTags}
 
 }
