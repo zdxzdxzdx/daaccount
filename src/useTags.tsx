aa0f720bc3b42311142af3fc37d91706671605d9
zdxzdxzdx <769371987@qq.com>
2021-02-24 14:55:40 +0800

    封装findTag到useTags

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -9,8 +9,10 @@ const defaultTags = [
 ]
 
 const useTags = ()=>{
-  const [tags, setTags] = useState<{ id:number,name:string}[]>(defaultTags
- );
-  return {tags,setTags}
+  const [tags, setTags] = useState<{ id:number,name:string}[]>(defaultTags);
+  const findTag = (id:number)=>
+    tags.filter(tag => tag.id===id)[0]
+
+  return {tags,setTags,findTag}
 }
 export {useTags}
--- src/views/Tag.tsx
+++ src/views/Tag.tsx
@@ -1,5 +1,15 @@
 import React from 'react';
-const Tag  =()=>{
-  return <div>hi</div>
+import {useTags} from '../useTags';
+import {
+  useParams
+} from "react-router-dom";
+type Params ={
+  id: string
+}
+const Tag:React.FC =(props)=>{
+  const {findTag}=useTags()
+  let { id } = useParams<Params>();
+  const tag =findTag(parseInt(id));
+  return <div>{tag.name}</div>
 }
 export {Tag}
