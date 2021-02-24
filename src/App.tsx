d0e26d13a59e24777202eafb62e6502b7492de17
zdxzdxzdx <769371987@qq.com>
2021-02-24 11:49:45 +0800

    创建/tags/:tag路由,exact精准匹配

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ import Statistics from 'views/Statistics';
 import Tags from 'views/Tags';
 import NoMatch from 'views/NoMatch';
 import styled from 'styled-components';
+import {Tag} from './views/Tag';
 
 const AppWrapper = styled.div `
    color: #333;
@@ -21,13 +22,16 @@ function App() {
    <AppWrapper>
     <Router>
       <Switch>
-        <Route path="/money">
+        <Route exact path="/money">
           <Money/>
         </Route>
-        <Route path="/tags">
+        <Route exact path="/tags">
           <Tags/>
         </Route>
-        <Route path="/statistics">
+        <Route exact  path="/tags/:tag" >
+          <Tag/>
+        </Route>
+        <Route exact path="/statistics">
           <Statistics/>
         </Route>
         <Redirect exact from="/" to="/money"/>
---
+++ src/views/Tag.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+const Tag  =()=>{
+  return <div>hi</div>
+}
+export {Tag}
