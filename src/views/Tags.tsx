913b4a8819d6a41e377617184bb6c2a260bc884b
zdxzdxzdx <769371987@qq.com>
2021-02-24 14:06:52 +0800

    修改路由的BUG

--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -44,7 +44,7 @@ function Tags() {
   return <Layout>
     <TagList>
       {tags.map(tag => <li key={tag.id}>
-        <Link to={'/tags/' + tag}>
+        <Link to={'/tags/' + tag.id}>
           <span className="oneLine">{tag.name}</span>
           <Icon name="right"/>
         </Link>
