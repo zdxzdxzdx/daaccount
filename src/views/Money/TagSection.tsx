f958c0f3304aea915fe0f4f863b93c74200b4e92
zdxzdxzdx <769371987@qq.com>
2021-02-24 13:54:09 +0800

    重构createId

--- src/views/Money/TagSection.tsx
+++ src/views/Money/TagSection.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import React from 'react';
 import {useTags} from '../../useTags';
+import {createId} from '../../lib/createId';
 
 
 const Wrapper = styled.section`
@@ -47,7 +48,7 @@ const TagSection: React.FC<(Props)> = (props) => {
   const onAddTag = () => {
     const tagName = window.prompt('新标签的名称为');
     if (tagName !== null) {
-      setTags([...tags, {id:Math.random(),name:tagName}]);
+      setTags([...tags, {id:createId(),name:tagName}]);
     }
 
   };
