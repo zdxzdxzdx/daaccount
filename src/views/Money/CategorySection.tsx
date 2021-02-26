94c83920ab151c93325986c8e3c9697611a390b0
zdxzdxzdx <769371987@qq.com>
2021-02-26 14:12:07 +0800

    修改背景颜色

--- src/views/Money/CategorySection.tsx
+++ src/views/Money/CategorySection.tsx
@@ -6,14 +6,13 @@ const Wrapper = styled.section`
     > ul{
         display: flex;
         background: #c4c4c4;
-        
+       
         > li {
           width: 50%;
           text-align: center;
           padding: 16px 0;
           position: relative;
-          //background: #F0DE38;
-          background: aliceblue;
+          background: #F0DE38;
            &.selected::after{
               content: '';
               display: block;
